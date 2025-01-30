cb79088021263d7ca473afa325ef792fedd57579
DamirMindiarov <akacuki1263547@gmail.com>
2025-01-30 16:23:27 +0700

    add action deploy

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,14 @@ import os
 
 from fabric import Connection, task
 
-print(os.environ["P_SSH_KEY"])
 
-
-# @task
-# def deploy(ctx):
-#     with Connection(
-#         "85.209.9.55",
-#         user="user",
-#         connect_kwargs={"key_filename": os.environ["P_SSH_KEY"]},
-#     ) as c:
-#         with c.cd("/home/user"):
-#             c.run("docker pull damirmin/test_fastapi:3.0.0")
-#             c.run("docker run -p 80:8000 -d damirmin/test_fastapi:3.0.0")
+@task
+def deploy(ctx):
+    with Connection(
+        "85.209.9.55",
+        user="user",
+        connect_kwargs={"key_filename": os.environ["P_SSH_KEY"]},
+    ) as c:
+        with c.cd("/home/user"):
+            c.run("docker pull damirmin/test_fastapi:3.0.0")
+            c.run("docker run -p 80:8000 -d damirmin/test_fastapi:3.0.0")
